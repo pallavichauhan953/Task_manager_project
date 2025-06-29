3fa61fec46c6fbac7284340b22c1338451d87756
Pallavi Chauhan <pallavichauhan2122@gmail.com>
2025-06-29 21:57:21 +0530

    Delete Task_Manager_Project directory

--- Task_Manager_Project/create_db.py
+++
@@ -1,6 +0,0 @@
-from app import db, app  # Import Flask app and SQLAlchemy db from app.py
-
-# Flask app ke context ke andar tables create karna zaroori hai
-with app.app_context():
-    db.create_all()
-    print("✅ Database and tables created successfully.")
